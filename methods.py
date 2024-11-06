74215d8733c4c100f521dda664d45f47092c2c17
Magnushhhh <s204245@dtu.dk>
2024-11-06 13:58:27 +0100

    SBM in methods and part 3 notebook

--- methods.py
+++ methods.py
@@ -154,6 +154,54 @@ def Dmatrix_Legendre(N,xjs,a,b):
         Vi = np.linalg.inv(V)
         D = dV@Vi * 2/(b-a) # shifted interval
         return D
+
+def SBM( P, ua, ub, ffun, method, tau = 1, d = 0):
+
+    r = JacobiGL(0,0,P)
+    f = ffun(r)
+    nvec = np.sqrt(2/(2*np.arange(P+1)+1))
+    V = JacobiP(r,0,0,P,matrix=True).T
+    Vn = V/nvec
+    Vr = GradJacobiP(r,0,0,P,matrix=True)
+    M = np.linalg.inv(Vn@Vn.T)
+    Vi = np.linalg.inv(V)
+    Vin = np.linalg.inv(Vn)
+    D = Vr@Vi
+
+    A = D.T @ M @ D 
+    b = M @ f
+
+    match method:
+        case 'strong_CBM':
+            A[0] = 0
+            A[0,0] = 1
+            b[0] = ua
+            A[-1] = 0
+            A[-1,-1] = 1
+            b[-1] = ub
+        case 'weak_CBM':
+            A[0] += D[0]
+            A[-1] -= D[-1]
+            A[0,0] += tau
+            A[-1,-1] += tau
+            b[0] += tau*ua
+            b[-1] += tau*ub
+        case 'weak_SBM':
+            Phi = JacobiP(np.array([-1-d,1+d]),0,0,P,matrix=True).T
+            H = (Phi @ Vi)
+
+            A[0] += D[0] + tau*H[0]
+            A[-1] += -D[-1] + tau*H[-1]
+            b[0] += tau*ua
+            b[-1] += tau*ub
+        case _:
+            print("No BC method.")
+            return
+    
+    u = np.linalg.solve(A,b)
+    condA = np.linalg.cond(A)
+    
+    return u, r, condA
     
 
 
