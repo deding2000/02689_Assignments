a22a1d15107060c8ccbd0a143af935e19fdb9856
deding2000 <chr2830@gmail.com>
2024-11-28 10:25:11 +0100

    added some initial functions

---
+++ Project/methods/ADR_RHS_1D.m
@@ -0,0 +1,17 @@
+function [rhsu] = ADR_RHS_1D(u,time, v,)
+% function [rhsu] = AdvecRHS1D(u,time)
+% Purpose : Evaluate RHS flux in 1D advection
+Globals1D;
+% form field differences at faces
+alpha=1;
+du = zeros(Nfp*Nfaces,K);
+du(:) = (u(vmapM)-u(vmapP)).*(v*nx(:)-(1-alpha)*abs(v*nx(:)))/2;
+% impose boundary condition at x=0
+uin = 0*time; %-sin(a*time);
+du (mapI) = (u(vmapI)- uin ).*(v*nx(mapI)-(1-alpha)*abs(v*nx(mapI)))/2;
+du (mapO) = 0;
+% compute right hand sides of the semi-discrete PDE
+advec = -a*rx.*(Dr*u) + LIFT*(Fscale.*(du));
+diff = D*rx.*(Dr*Dr*u) + LIFT*(Fscale.*(du));
+rhsu = advec;
+return
---
+++ Project/methods/Globals1D.m
@@ -0,0 +1,13 @@
+global N Nfp Np K
+global r x VX
+global Dr LIFT
+global nx Fx Fscale
+global vmapM vmapP vmapB mapB Fmask
+global vmapI vmapO mapI mapO
+global rx J
+global rk4a rk4b rk4c
+global Nfaces EToE EToF
+global V invV
+global NODETOL
+% Parameters of model
+global D v lambda R
